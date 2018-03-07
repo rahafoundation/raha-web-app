df3c0b4c6ab3aabd9c0d8b62f5a9d7884ee47548
ataki <jim32990@gmail.com>
2018-03-07 11:55:16 -0800

    add seedFn for easier testing

--- src/members.js
+++ src/members.js
@@ -1,8 +1,9 @@
-const getNumberSuffix = (len: number) => {
+const getNumberSuffix = (len, seedFn) => {
     const exclusiveMax = Math.pow(10, len);
-    return (Math.floor(Math.random() * exclusiveMax) + exclusiveMax).toString().substring(1);
+    return (Math.floor(seedFn() * exclusiveMax) + exclusiveMax).toString().substring(1);
 }
 
-export const getMemberId = (displayName: string) => {
-    return displayName.trim().toLowerCase().replace(/\s+/g, '.') + '$' + getNumberSuffix(4);
+export const getMemberId = (displayName, seedFn) => {
+    seedFn = seedFn || Math.random
+    return displayName.trim().toLowerCase().replace(/\s+/g, '.') + '$' + getNumberSuffix(4, seedFn);
 }
