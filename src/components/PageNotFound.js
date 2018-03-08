adb905d20cf7e52e6426809579de26d3506f24d6
kem226 <kristinelizmurray@gmail.com>
2018-03-07 21:38:28 -0800

    add localization

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
 import RahaTitle from './RahaTitle';
 import { Link } from 'react-router-dom';
+import { FormattedMessage } from 'react-intl';
+import STRINGS from '../strings';
 
 const PageNotFound = () => (
   <div className="PageNotFound">
     <RahaTitle />
-    <p><strong>404</strong> page not found, go <Link to="/">home</Link>.</p>
+     <FormattedMessage {...STRINGS.page_not_found} 
+     values={{404: <strong>404</strong>, home: <Link to="/">home</Link>}} />
     <p>¯\_(ツ)_/¯</p>
   </div>
 );
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { IntlProvider } from 'react-intl'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<IntlProvider locale="en"><App /></IntlProvider>, document.getElementById('root'));
