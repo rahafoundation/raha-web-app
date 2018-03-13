cb469d1c771a6028a6081ab0259df8b1ef1c9e83
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-03-13 16:47:28 -0700

    Redirect login if already logged in.
    /login redirects to the profile page if the user is already logged in.
    
    /me redirects to login. This has the indirect effect of /me redirecting
    to the profile page if the user is already logged in.

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,21 +1,37 @@
 import * as React from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
 import * as firebase from 'firebase';
 import { FirebaseAuth } from 'react-firebaseui';
-import { auth } from '../firebaseInit';
 
-const LogIn = ({ noRedirect }) => {
+import { auth } from '../firebaseInit';
+import { getAuthMemberDoc } from '../connectors';
+
+const LogIn = ({ authMemberDoc, noRedirect }) => {
   const uiConfig = {
     signInFlow: 'popup',
     signInOptions: [
       firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       firebase.auth.GithubAuthProvider.PROVIDER_ID,
-    ]
+    ], callbacks: {
+      signInSuccess: () => {
+        return false;
+      }
+    },
   };
-  if (!noRedirect) {
-    uiConfig.signInSuccessUrl = '/me';
+  if (authMemberDoc) {
+    return (
+      <Redirect to={`/m/${authMemberDoc.get('mid')}`} replace />
+    )
+  } else {
+    return <FirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />;
   }
-  return <FirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />;
 };
 
-export default LogIn;
+function mapStateToProps(state, ownProps) {
+  const authMemberDoc = getAuthMemberDoc(state);
+  return { authMemberDoc };
+}
+
+export default connect(mapStateToProps)(LogIn);
