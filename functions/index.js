fa70b97610c4072cf05eb141f7bf33ac0c2e6d0e
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-03-13 22:27:06 -0700

    Revert back to just using js for functions.

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,5 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
-import { getPrefixFromMemberId, getSuffixFromMemberId } from './members';
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
@@ -39,10 +37,6 @@ function createMemberFromOperation(operation) {
         full_name,
         invite_confirmed: false,
         mid: creator_mid,
-        // Are the following two fields necessary? Getting rid of them
-        // would also free us from all the weirdness around copying members.ts.
-        mid_prefix: getPrefixFromMemberId(creator_mid),
-        mid_suffix: getSuffixFromMemberId(creator_mid),
         request_invite_block_at: block_at,
         request_invite_block_seq: block_seq,
         request_invite_op_seq: op_seq,
@@ -52,7 +46,7 @@ function createMemberFromOperation(operation) {
     });
 }
 
-export const onCreateOperation = functions.firestore
+exports.onCreateOperation = functions.firestore
     .document('/operations/{operationId}')
     .onCreate((event) => {
         const newOperation = event.data.data();
