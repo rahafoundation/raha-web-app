4dc7fcd0053f41e54954a8bf7a1bfa282397284f
Omar Diab <me@omardiab.com>
2018-03-22 21:04:25 -0700

    restyle ActionButton

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import styled from 'styled-components';
+import { green300, green500 } from 'material-ui/styles/colors';
+
 import { getAuthMemberDoc } from '../connectors';
 import { getTrustOperation } from '../operations';
 import { postOperation } from '../actions';
@@ -11,6 +14,22 @@ interface Props {
   toUid: string;
   toMid: string;
 }
+
+const ButtonElem = styled.button`
+  cursor: pointer;
+  color: white;
+  background: ${green500};
+  transition: background-color .25s;
+
+  font-size: 1rem;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 2px;
+
+  :hover, :active, :focus {
+    background: ${green300};
+  }
+`;
 
 // TODO(#8) instead of only trust, should be multi-functional button
 class ActionButton extends Component<Props> {
@@ -30,9 +49,17 @@ class ActionButton extends Component<Props> {
       return <Redirect to={`/m/${this.props.toMid}/invite`} />
     }
     if (this.props.authMemberDoc.id && this.props.authMemberDoc.get('mid')) {
-      return <button className="ActionButton" onClick={this.onTrustClick}>Trust</button>;
+      return (
+        <ButtonElem onClick={this.onTrustClick}>
+          Trust
+        </ButtonElem>
+      );
     } else {
-      return <button className="ActionButton" onClick={this.onInviteClick}>Share Invite Video to Join</button>;
+      return (
+        <ButtonElem onClick={this.onInviteClick}>
+          Share Invite Video to Join
+        </ButtonElem>
+      );
     }
   }
 }
