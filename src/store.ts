4772dd9cd66be35568455f98f9f6677b13e92c6b
Omar Diab <me@omardiab.com>
2018-04-02 14:58:15 -0700

    add redux devtools extension to app

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  composeWithDevTools(applyMiddleware(thunk))
 );
 
 export { AppState } from './reducers';
