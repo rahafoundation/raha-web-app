5b97b604ae5e30bc5c3c55cf142ef17266a7bc5c
Omar Diab <me@omardiab.com>
2018-04-03 23:04:34 -0700

    add setup tests file

---
+++ src/setupTests.ts
@@ -0,0 +1,6 @@
+import 'jest-localstorage-mock';
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+
+// Setup adapter to work with enzyme 3.2.0
+Enzyme.configure({ adapter: new Adapter() });
