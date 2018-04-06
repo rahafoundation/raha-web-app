addd7ad3a66fa7e9cd11ba31229d36c5db77c151
Omar Diab <me@omardiab.com>
2018-04-06 13:10:44 -0700

    split reducers up for simplicity

---
+++ src/reducers/modal.ts
@@ -0,0 +1,32 @@
+import { Reducer } from "redux";
+
+import {
+  HIDE_MODAL,
+  HideModalAction,
+  SHOW_MODAL,
+  ShowModalAction
+} from "../actions";
+
+export interface ModalState {
+  element: React.ReactNode | null;
+}
+type ModalAction = HideModalAction | ShowModalAction;
+
+const modal: Reducer<ModalState> = (
+  state = { element: null }, untypedAction
+) => {
+  // TODO: this is a hack for now until Redux 4's typing comes out, or we use
+  // a flux standard actions library to handle typings; this gives stronger
+  // type checking.
+  const action = untypedAction as ModalAction;
+  switch (action.type) {
+    case HIDE_MODAL:
+      return { ...state, element: null };
+    case SHOW_MODAL:
+      return { ...state, element: action.element };
+    default:
+      return state;
+  }
+};
+
+export default modal;
