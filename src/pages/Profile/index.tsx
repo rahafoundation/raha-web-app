87147b67867bb2c146f24aca540d998e61de9b59
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-04-12 15:42:58 -0700

    Autoformat.

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 
 import { AppState } from "../../store";
 import { Uid, Mid } from "../../identifiers";
-import { Member, UidSet, GENESIS_USER } from "../../reducers/membersNew"
+import { Member, UidSet, GENESIS_USER } from "../../reducers/membersNew";
 import MemberRelations from "./MemberRelations";
 
 import ActionButton from "../../components/ActionButton";
@@ -17,7 +17,7 @@ import InviteVideo from "../../components/InviteVideo";
  * ================
  */
 interface OwnProps {
-  match: { params: { memberMid: Mid }}
+  match: { params: { memberMid: Mid } };
 }
 
 interface StateProps {
@@ -28,7 +28,7 @@ interface StateProps {
     trustedByMembers: Member[];
     invitedMembers: Member[];
     invitedByMember: Member | typeof GENESIS_USER;
-  }
+  };
 }
 type Props = OwnProps & StateProps;
 
@@ -36,13 +36,20 @@ type Props = OwnProps & StateProps;
  * Data helpers
  * =============
  */
-function isOwnProfile(loggedInMember: Member | undefined, profileMember: Member): boolean {
+function isOwnProfile(
+  loggedInMember: Member | undefined,
+  profileMember: Member
+): boolean {
   return !!loggedInMember && loggedInMember.uid === profileMember.uid;
 }
 
-function canTrustUser(loggedInMember: Member | undefined, toTrust: Member): boolean {
+function canTrustUser(
+  loggedInMember: Member | undefined,
+  toTrust: Member
+): boolean {
   return (
-    !!loggedInMember && !isOwnProfile(loggedInMember, toTrust) &&
+    !!loggedInMember &&
+    !isOwnProfile(loggedInMember, toTrust) &&
     !loggedInMember.trusts[toTrust.uid]
   );
 }
@@ -53,7 +60,10 @@ function canTrustUser(loggedInMember: Member | undefined, toTrust: Member): bool
  * b) the user has been trusted by the person inviting them.
  */
 function isInviteConfirmed(profileMember: Member): boolean {
-  return profileMember.invitedBy === GENESIS_USER || profileMember.invitedBy in profileMember.trustedBy;
+  return (
+    profileMember.invitedBy === GENESIS_USER ||
+    profileMember.invitedBy in profileMember.trustedBy
+  );
 }
 
 /* ==================
@@ -80,15 +90,16 @@ const ProfileElem = styled.main`
 /**
  * Presentational component for displaying a profile.
  */
-const ProfileView: React.StatelessComponent<Props> = (props) => {
-  const {
-    profileData, loggedInMember
-  } = props;
+const ProfileView: React.StatelessComponent<Props> = props => {
+  const { profileData, loggedInMember } = props;
   if (!profileData) {
-    return <Loading />
+    return <Loading />;
   }
   const {
-    profileMember, trustedByMembers, trustedMembers, invitedByMember,
+    profileMember,
+    trustedByMembers,
+    trustedMembers,
+    invitedByMember,
     invitedMembers
   } = profileData;
   const inviteConfirmed = isInviteConfirmed(profileMember);
@@ -108,7 +119,7 @@ const ProfileView: React.StatelessComponent<Props> = (props) => {
             />
           )}
         </h1>
-        { /* TODO: This component looks like it has extraneous deps */ }
+        {/* TODO: This component looks like it has extraneous deps */}
         <TrustLevel
           ownProfile={isOwnProfile(loggedInMember, profileMember)}
           trustLevel={inviteConfirmed ? 3 : 0}
@@ -123,7 +134,9 @@ const ProfileView: React.StatelessComponent<Props> = (props) => {
           // TODO: should be using mid
           // TODO: should be using internationalized message
           <InviteVideo memberId={profileMember.mid} />
-        ) : <div>Pending trust confirmation before showing public video</div>}
+        ) : (
+          <div>Pending trust confirmation before showing public video</div>
+        )}
 
         <MemberRelations
           invitedByMember={invitedByMember}
@@ -134,50 +147,57 @@ const ProfileView: React.StatelessComponent<Props> = (props) => {
       </main>
     </ProfileElem>
   );
-}
+};
 
 /* ================
  * Redux container
  * ================
  */
 
- // TODO: make this a selector on the state
- // TODO: keep a lookup table by Mid, not just Uid, to make this efficient
+// TODO: make this a selector on the state
+// TODO: keep a lookup table by Mid, not just Uid, to make this efficient
 function getMemberByMid(state: AppState, mid: Mid): Member | undefined {
-  return Object.values(state.membersNew).find(member => member.mid === mid)
+  return Object.values(state.membersNew).find(member => member.mid === mid);
 }
 
 // TODO: make this a selector on the state, not inline in this container
 function getMembersFromUidSet(state: AppState, uids: UidSet): Member[] {
-  return Object.keys(uids).map(uid => state.membersNew[uid])
+  return Object.keys(uids).map(uid => state.membersNew[uid]);
 }
 
 // TODO: make this a selector on the state, not inline in this container
 function getLoggedInMember(state: AppState) {
-  const loggedInFirebaseUid = state.auth.firebaseUser !== null ? state.auth.firebaseUser.uid : undefined;
-  return loggedInFirebaseUid ? state.membersNew[loggedInFirebaseUid] : undefined
+  const loggedInFirebaseUid =
+    state.auth.firebaseUser !== null ? state.auth.firebaseUser.uid : undefined;
+  return loggedInFirebaseUid
+    ? state.membersNew[loggedInFirebaseUid]
+    : undefined;
 }
 
-const mapStateToProps: MapStateToProps<StateProps, OwnProps, AppState> =
-  (state, ownProps) => {
-    const loggedInMember = getLoggedInMember(state);
-    const profileMember = getMemberByMid(state, ownProps.match.params.memberMid)
-    if (!profileMember) {
-      return { loggedInMember };
-    }
-
-    const invitedByMember = profileMember && profileMember.invitedBy === GENESIS_USER ?
-      GENESIS_USER : state.membersNew[profileMember.invitedBy]
-    return {
-      loggedInMember,
-      profileData: {
-        profileMember,
-        trustedMembers: getMembersFromUidSet(state, profileMember.trusts),
-        trustedByMembers: getMembersFromUidSet(state, profileMember.trustedBy),
-        invitedMembers: getMembersFromUidSet(state, profileMember.invited),
-        invitedByMember
-      }
-    }
+const mapStateToProps: MapStateToProps<StateProps, OwnProps, AppState> = (
+  state,
+  ownProps
+) => {
+  const loggedInMember = getLoggedInMember(state);
+  const profileMember = getMemberByMid(state, ownProps.match.params.memberMid);
+  if (!profileMember) {
+    return { loggedInMember };
   }
+
+  const invitedByMember =
+    profileMember && profileMember.invitedBy === GENESIS_USER
+      ? GENESIS_USER
+      : state.membersNew[profileMember.invitedBy];
+  return {
+    loggedInMember,
+    profileData: {
+      profileMember,
+      trustedMembers: getMembersFromUidSet(state, profileMember.trusts),
+      trustedByMembers: getMembersFromUidSet(state, profileMember.trustedBy),
+      invitedMembers: getMembersFromUidSet(state, profileMember.invited),
+      invitedByMember
+    }
+  };
+};
 
 export default connect(mapStateToProps)(ProfileView);
