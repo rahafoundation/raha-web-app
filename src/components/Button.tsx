e56c40354d4923623982ca7b84a5d68d37ca1193
Omar Diab <me@omardiab.com>
2018-04-13 04:40:33 -0500

    support disabled buttons

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,12 @@ import styledUntyped, { ThemedBaseStyledInterface } from "styled-components";
 
 import { postOperation } from "../actions";
 import { getAuthMemberDoc } from "../connectors";
-import { interactive } from "../constants/palette";
+import { interactive, gray } from "../constants/palette";
 import { MemberDoc } from "../members";
 import { getTrustOperation } from "../operations";
 import { AppState } from "../store";
 import { linkStyles } from "./Link";
+import { white } from "material-ui/styles/colors";
 
 // TODO code duplication in functions/srs/index.ts, decomp.
 
@@ -24,36 +25,41 @@ import { linkStyles } from "./Link";
  * LINK is rendered similarly to a hyperlink.
  */
 export enum ButtonType {
-  PRIMARY = 'primary',
-  SECONDARY = 'secondary',
-  LINK = 'link'
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+  LINK = "link"
 }
 
 export enum ButtonSize {
-  SMALL = 'small',
-  REGULAR = 'regular',
-  LARGE = 'large'
+  SMALL = "small",
+  REGULAR = "regular",
+  LARGE = "large"
 }
 
 interface Props {
-  type?: ButtonType
-  size?: ButtonSize
-  children: React.ReactNode
-  onClick: () => void
-  className?: string
-  style?: React.CSSProperties
+  type?: ButtonType;
+  size?: ButtonSize;
+  children: React.ReactNode;
+  onClick: () => void;
+  className?: string;
+  style?: React.CSSProperties;
+  disabled?: boolean;
 }
 
 const buttonStyles = {
   background: {
     [ButtonType.PRIMARY]: interactive.primary,
     [ButtonType.SECONDARY]: interactive.secondary,
-    [ButtonType.LINK]: "none"
+    [ButtonType.LINK]: "none",
+    disabled: gray,
+    disabledLink: "none"
   },
   color: {
-    [ButtonType.PRIMARY]: "white",
-    [ButtonType.SECONDARY]: "white",
-    [ButtonType.LINK]: linkStyles.color
+    [ButtonType.PRIMARY]: white,
+    [ButtonType.SECONDARY]: white,
+    [ButtonType.LINK]: linkStyles.color,
+    disabled: white,
+    disabledLink: gray
   },
   size: {
     [ButtonSize.SMALL]: ".8rem",
@@ -83,41 +89,61 @@ const buttonStyles = {
       [ButtonType.LINK]: linkStyles.hover.textDecoration
     }
   }
-}
+};
 
 interface ThemeProps {
   type: ButtonType;
   size: ButtonSize;
 }
-const styled = styledUntyped as ThemedBaseStyledInterface<ThemeProps>
+const styled = styledUntyped as ThemedBaseStyledInterface<ThemeProps>;
 const ButtonElem = styled.button`
-  cursor: pointer;
-  padding: ${props => props.theme.type === ButtonType.LINK ?
-    0 : buttonStyles.padding[props.theme.size]
-  };
+  cursor: ${props => (props.disabled ? "not-allowed" : "pointer")};
+  padding: ${props =>
+    props.theme.type === ButtonType.LINK
+      ? 0
+      : buttonStyles.padding[props.theme.size]};
   border: none;
   border-radius: 2px;
 
-  color: ${props => buttonStyles.color[props.theme.type]};
-  background: ${props => buttonStyles.background[props.theme.type]};
+  color: ${props =>
+    props.disabled
+      ? props.theme.type === ButtonType.LINK
+        ? buttonStyles.color.disabledLink
+        : buttonStyles.color.disabled
+      : buttonStyles.hover.color[props.theme.type]};
+  background: ${props =>
+    props.disabled
+      ? props.theme.type === ButtonType.LINK
+        ? buttonStyles.background.disabledLink
+        : buttonStyles.background.disabled
+      : buttonStyles.hover.background[props.theme.type]};
   text-decoration: none;
 
   font-size: ${props => buttonStyles.size[props.theme.size]};
-  font-weight: ${props => props.theme.type === ButtonType.LINK ?
-    linkStyles.fontWeight : "500"
-  };
+  font-weight: ${props =>
+    props.theme.type === ButtonType.LINK ? linkStyles.fontWeight : "500"};
 
   transition: color 0.15s, text-decoration 0.15s, background-color 0.15s;
 
   :hover,
   :active,
   :focus {
-    background: ${props => buttonStyles.hover.background[props.theme.type]};
-    color: ${props => buttonStyles.hover.color[props.theme.type]}
+    background: ${props =>
+      props.disabled
+        ? props.theme.type === ButtonType.LINK
+          ? buttonStyles.background.disabledLink
+          : buttonStyles.background.disabled
+        : buttonStyles.background[props.theme.type]};
+    color: ${props =>
+      props.disabled
+        ? props.theme.type === ButtonType.LINK
+          ? buttonStyles.color.disabledLink
+          : buttonStyles.color.disabled
+        : buttonStyles.color[props.theme.type]};
     text-decoration: ${props =>
-      props.theme.type === ButtonType.LINK ?
-        buttonStyles.hover.textDecoration[props.theme.type] : "none"
-    };
+      props.theme.type === ButtonType.LINK
+        ? buttonStyles.hover.textDecoration[props.theme.type]
+        : "none"};
   }
 `;
 
@@ -126,22 +152,25 @@ const ButtonElem = styled.button`
  * Not intended for webpage linking, but instead for running
  * code on click.
  */
-const Button: React.StatelessComponent<Props> = (props) => {
-  const {
-    className, style, onClick, children
-  } = props;
+const Button: React.StatelessComponent<Props> = props => {
+  const { className, style, onClick, children } = props;
   const type = props.type ? props.type : ButtonType.PRIMARY;
   const size = props.size ? props.size : ButtonSize.REGULAR;
+  const disabled = props.disabled ? props.disabled : false;
 
   return (
     <ButtonElem
-      onClick={(e) => { e.preventDefault(); onClick(); }}
+      onClick={e => {
+        e.preventDefault();
+        onClick();
+      }}
       className={className}
       style={style}
-      theme={{type, size}}
+      theme={{ type, size }}
+      disabled={disabled}
     >
       {children}
     </ButtonElem>
   );
-}
+};
 export default Button;
--- src/constants/palette.ts
+++ src/constants/palette.ts
@@ -5,14 +5,14 @@ import {
   cyanA700,
   green500,
   lightGreen500
-} from 'material-ui/styles/colors';
+} from "material-ui/styles/colors";
 
 export const green = green500;
 export const lightGreen = lightGreen500;
-export const gray = 'gray';
+export const gray = "gray";
 export const interactive = {
-  primary: cyanA700,
-  primaryHover: cyanA400,
-  secondary: blueGrey300,
-  secondaryHover: blueGrey100
+  primary: cyanA400,
+  primaryHover: cyanA700,
+  secondary: blueGrey100,
+  secondaryHover: blueGrey300
 };
