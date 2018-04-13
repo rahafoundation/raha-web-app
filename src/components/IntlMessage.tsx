834714fbddc54122b6d64dbf7667817713866b66
Omar Diab <me@omardiab.com>
2018-04-13 04:41:05 -0500

    format intlmessage

--- src/components/IntlMessage.tsx
+++ src/components/IntlMessage.tsx
@@ -1,28 +1,30 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import { FormattedMessage, MessageValue } from 'react-intl';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { FormattedMessage, MessageValue } from "react-intl";
 
 interface BaseProps {
   id: string;
-  values?: {[key: string]: MessageValue | JSX.Element};
-};
+  values?: { [key: string]: MessageValue | JSX.Element };
+}
 
-type ClassNameRenderers = { className?: string } & (
-  { tagName?: keyof JSX.IntrinsicElements }
-);
-type IndependentRenderers = (
-  { onlyRenderText: boolean } |
-  { render: (message: string[]) => React.ReactNode }
-);
+type ClassNameRenderers = { className?: string } & ({
+  tagName?: keyof JSX.IntrinsicElements;
+});
+type IndependentRenderers =
+  | { onlyRenderText: boolean }
+  | { render: (message: string[]) => React.ReactNode };
 
-type Props = BaseProps | (BaseProps & (
-  ClassNameRenderers | IndependentRenderers
-));
+type Props =
+  | BaseProps
+  | (BaseProps & (ClassNameRenderers | IndependentRenderers));
 
-const IntlMessage: React.StatelessComponent<Props> = (props) => {
-  const baseProps = { id: props.id, ...("values" in props ? {values: props.values} : {})};
+const IntlMessage: React.StatelessComponent<Props> = props => {
+  const baseProps = {
+    id: props.id,
+    ...("values" in props ? { values: props.values } : {})
+  };
   return (
-    <FormattedMessage {...baseProps} >
+    <FormattedMessage {...baseProps}>
       {(message: string[]) => {
         if ("onlyRenderText" in props && props.onlyRenderText) {
           return message;
@@ -30,13 +32,15 @@ const IntlMessage: React.StatelessComponent<Props> = (props) => {
         if ("render" in props) {
           return props.render(message);
         }
-        const tagName = "tagName" in props && props.tagName !== undefined ?
-          props.tagName : "span";
+        const tagName =
+          "tagName" in props && props.tagName !== undefined
+            ? props.tagName
+            : "span";
         const className = "className" in props ? props.className : undefined;
         return React.createElement(tagName, { className });
-    }}
+      }}
     </FormattedMessage>
   );
-}
+};
 
 export default IntlMessage;
