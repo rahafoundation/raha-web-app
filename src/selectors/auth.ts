f5d9ace9ebe5f31bce3200057b476befe261d279
Omar Diab <me@omardiab.com>
2018-04-13 17:35:44 -0500

    add helpful selectors

--- src/selectors/auth.ts
+++ src/selectors/auth.ts
@@ -1,13 +1,22 @@
 import { AppState } from "../store";
 
 export async function getAuthToken(
-  getState: () => AppState
+  state: AppState
 ): Promise<string | undefined> {
-  const authFirebaseUser = getState().auth.firebaseUser;
+  const authFirebaseUser = state.auth.firebaseUser;
   if (!authFirebaseUser) {
     // TODO: trigger login or error
     return;
   }
   const authToken = await authFirebaseUser.getIdToken();
   return authToken;
+}
+
+// TODO: make this a selector on the state, not inline in this container
+export function getLoggedInMember(state: AppState) {
+  const loggedInFirebaseUid =
+    state.auth.firebaseUser !== null ? state.auth.firebaseUser.uid : undefined;
+  return loggedInFirebaseUid
+    ? state.membersNew.byUid[loggedInFirebaseUid]
+    : undefined;
 }
