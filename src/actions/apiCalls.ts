69777338343ad8e05ddb466e631a699c07d60b14
Omar Diab <me@omardiab.com>
2018-04-16 17:30:03 -0400

    fix apiCall error handling logic to fail even on uncaught errors

--- src/actions/apiCalls.ts
+++ src/actions/apiCalls.ts
@@ -66,17 +66,20 @@ export const wrapApiCallAction: (
       dispatch(successAction);
       return result;
     } catch (err) {
-      if (err instanceof ApiCallError) {
-        const failureAction: ApiCallsAction = {
-          type: ApiCallsActionType.FAILURE,
-          endpoint,
-          identifier,
-          error: err
-        };
-        dispatch(failureAction);
-        return;
+      // tslint:disable-next-line:no-console
+      console.error("API Call failed: ", err);
+      const failureAction: ApiCallsAction = {
+        type: ApiCallsActionType.FAILURE,
+        endpoint,
+        identifier,
+        error: err
+      };
+      dispatch(failureAction);
+
+      // continue throwing unrelated errors
+      if (!(err instanceof ApiCallError)) {
+        throw err;
       }
-      throw err;
     }
   };
 };
