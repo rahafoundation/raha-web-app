41f8862af6e3264eaecc01e6222492170c9e8edc
Omar Diab <me@omardiab.com>
2018-04-18 16:17:11 -0500

    add relevant translations for request_invite

--- src/data/locales/en.ts
+++ src/data/locales/en.ts
@@ -2,10 +2,26 @@ export default {
   "profile.trustButton": "Trust",
   "profile.trustedMember": "Trusted",
   "profile.trustFailure": "Trusting failed, try again",
+
+  "request_invite.step0":
+    "{inviter_name} invited you to Raha.io Network, an identity network supporting universal basic income.",
+  "request_invite.step1":
+    "We believe every life has value, but financialization and automation is creating unsustainable and unethical inequality.",
+  "request_invite.step2":
+    "Members of the Raha identity network receive a basic income of Raha coin. In exchange, when you are inactive for a year or more then your coin are donated back to the network.",
+  "request_invite.step3":
+    "Raha is free to join with 0 transaction fees. Talk to {inviter_name} to create your video identity!",
+
+  "request_invite.agreements.inactivityDonation":
+    "I understand and agree that after 1 year of inactivity all of my Raha will be irrevocably and irretrievably donated to fund basic income, with 80% going directly to members and 20% to the member-owned Raha Foundation.",
+  "request_invite.agreements.communityStandards":
+    "I agree to the community {code_of_conduct}, {terms_of_service}, and {privacy_policy}.",
+  "request_invite.agreements.realIdentity":
+    "This is my real identity, my full name, and only time I have joined Raha. I understand that creating duplicate or fake accounts may result in me and people I have invited losing access to our accounts.",
+  "request_invite.agreements.age": "I am at least 13 years old.",
+
   "app_layout.log_in": "Log In",
   completely_free: "completely free",
-  invite_me_intro:
-    "We are excited to have join as well! Joining is {completely_free}, but like {full_name} was, you must be invited by an existing member in person via video using your full name and help maintain the identity network. Part of the value of Raha.io Network is that it's a unique identity platform. If you sign up a fake identity or have multiple accounts, or invite people with fake/duplicate accounts, your account will be frozen. If you know of any fake accounts, report them to increase your income level! Only accept an invite if you trust this member and share similar values, because they will be your default admin in the event you need to recover your account and default representantive to select your vote for the Raha.io Board. If it turns out they invited many fake or duplicate accounts, then your account is at risk of being flagged.",
   invite_others_instructions:
     "Hi there {full_name}! If you would like to invite someone else, make a video where each of you say your full name and (optionally) why you want to join Raha, have them upload it to Youtube, then direct them to visit {invite_link} to upload their own video and make their account. After they do this, you will have to go visit their profile, check the video is accurate, and hit Trust for them to become members. We are working to make this process more convenient, you have any thoughts reach us at {ideas_email} or file a {github_issue}.",
   invite_pending:
@@ -21,10 +37,8 @@ export default {
   own_invite_page: "This is what your invite page looks like to others:",
   page_not_found: "{404} page not found, go {home}.",
   upload_invite: "Upload your public invite video! Max 60 MB.",
-  request_invite:
-    "Request Invite from {full_name} ({mid}). Here is their invite video:",
-  sign_up_above:
-    "Sign up above to continue. We do not ask for your contact's information or ability to post.",
+  sign_up:
+    "Sign up below to continue. We do not ask for your contact's information or ability to post.",
   "request_invite.code_of_conduct": "Code of Conduct",
   "request_invite.privacy_policy": "Privacy Policy",
   "request_invite.terms_of_service": "Terms of Service",
