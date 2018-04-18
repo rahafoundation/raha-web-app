80a38c4dde2fba76acbb9f59262f8b3c0c1d7062
Omar Diab <me@omardiab.com>
2018-04-18 16:17:33 -0500

    work in progress on the new steps

--- src/pages/RequestInvite/index.tsx
+++ src/pages/RequestInvite/index.tsx
@@ -3,6 +3,7 @@ import { FormattedMessage } from "react-intl";
 import { connect, MapStateToProps, MergeProps } from "react-redux";
 import styled from "styled-components";
 
+import { Uid } from "../../identifiers";
 import { requestInviteFromMember } from "../../actions";
 import { getPrivateVideoInviteRef } from "../../connectors";
 import { getMemberId, MemberDoc } from "../../members";
@@ -15,8 +16,27 @@ import InviteVideo from "../../components/InviteVideo";
 import Loading from "../../components/Loading";
 import Video from "../../components/Video";
 import VideoUploader from "../../components/VideoUploader";
+import Button from "../../components/Button";
+import IntlMessage from "../../components/IntlMessage";
+import { Redirect } from "react-router-dom";
+import { lightGreen } from "../../constants/palette";
+import { lightBlue500, blueGrey300 } from "material-ui/styles/colors";
 
 const RequestInviteElem = styled.main`
+  > header {
+    text-align: center;
+
+    > .ownInvitePage {
+      display: inline-block;
+      background: ${blueGrey300};
+      color: white;
+      max-width: 80vw;
+      text-align: center;
+      padding: 10px 20px;
+      margin-bottom: 20px;
+    }
+  }
+
   > section,
   > form,
   > .inviteVideo {
@@ -58,10 +78,10 @@ interface OwnProps {
 }
 
 interface StateProps {
-  loggedInFullName: string | undefined | null;
-  loggedInFirebaseUser: { uid: string } | null;
+  loggedInFullName?: string;
+  loggedInFirebaseUser?: { uid: string };
 
-  requestingFromMember: Member | undefined;
+  requestingFromMember?: Member;
 
   isLoading: boolean;
   isOwnInvitePage: boolean;
@@ -191,28 +211,7 @@ export class RequestInvite extends React.Component<Props, State> {
           setVideoUrl={videoUrl => this.setState({ videoUrl })}
           uploadRef={uploadRef}
         />
-        <div>
-          <FormattedMessage
-            id="request_invite.terms"
-            values={{
-              code_of_conduct: (
-                <a href="/code-of-conduct">
-                  <FormattedMessage id="request_invite.code_of_conduct" />
-                </a>
-              ),
-              privacy_policy: (
-                <a href="/privacy-policy">
-                  <FormattedMessage id="request_invite.privacy_policy" />
-                </a>
-              ),
-              terms_of_service: (
-                <a href="/terms-of-service">
-                  <FormattedMessage id="request_invite.terms_of_service" />
-                </a>
-              )
-            }}
-          />
-        </div>
+        <div />
         <br />
         <button className="InviteButton Green" onClick={this.handleOnSubmit}>
           {/* TODO: internationalize */}
@@ -234,66 +233,33 @@ export class RequestInvite extends React.Component<Props, State> {
   }
 
   public render() {
-    if (this.props.isLoading) {
+    const { requestingFromMember, loggedInFirebaseUser } = this.props;
+    if (!requestingFromMember || !loggedInFirebaseUser) {
       return <Loading />;
     }
 
-    const requestingFromMember = this.props.requestingFromMember as Member;
     // TODO check if user already invited
     if (this.state.submitted) {
-      // TODO we should instead redirect to profileUrl, which should display this message along with their invite video.
-      const profileUrl = `${window.location.origin}/m/${this.state.creatorMid}`;
-      return (
-        <div>
-          Your video has been submitted for review! After approval by us and{" "}
-          {requestingFromMember.fullName}, your profile will appear at{" "}
-          <a href={profileUrl}>{profileUrl}</a>. We are available at{" "}
-          <a href="mailto:help@raha.io">help@raha.io</a> if you have any
-          questions.
-        </div>
-      );
+      const profileUrl = `${window.location.origin}`;
+      return <Redirect to={profileUrl} />;
     }
+
+    const videoUploadRef =
+      storageRef &&
+      getPrivateVideoInviteRef(storageRef, loggedInFirebaseUser.uid);
 
     return (
       <RequestInviteElem>
         {this.props.isOwnInvitePage && (
-          <section>
-            <FormattedMessage id="own_invite_page" />
-          </section>
+          <header>
+            <IntlMessage id="own_invite_page" className="ownInvitePage" />
+          </header>
         )}
-        <section className="requestInviteMessage">
-          <FormattedMessage
-            id="request_invite"
-            values={{
-              full_name: requestingFromMember.fullName,
-              mid: requestingFromMember.mid
-            }}
-          />
-        </section>
-        <InviteVideo
-          className="inviteVideo"
-          memberId={requestingFromMember.mid}
+        <WelcomeSteps
+          inviterName={requestingFromMember.fullName}
+          hasLoggedIn={!!loggedInFirebaseUser}
+          videoUploadRef={videoUploadRef}
         />
-        <section>
-          <FormattedMessage
-            id="invite_me_intro"
-            values={{
-              full_name: requestingFromMember.fullName,
-              completely_free: (
-                <span className="completelyFree">
-                  <FormattedMessage id="completely_free" />
-                </span>
-              )
-            }}
-          />
-        </section>
-        {this.props.isLoading ? (
-          <Loading />
-        ) : this.props.loggedInFirebaseUser ? (
-          this.renderForm()
-        ) : (
-          this.renderLogIn()
-        )}
       </RequestInviteElem>
     );
   }
@@ -303,13 +269,13 @@ const mapStateToProps: MapStateToProps<StateProps, OwnProps, AppState> = (
   state: AppState,
   ownProps: OwnProps
 ) => {
-  const isAuthLoaded = state.auth.isLoaded;
   const loggedInFirebaseUser = state.auth.firebaseUser;
+  const isAuthLoaded = !!state.auth.firebaseUser;
   const loggedInMember: Member | undefined = loggedInFirebaseUser
     ? state.membersNew.byUid[loggedInFirebaseUser.uid]
     : undefined;
   // TODO: Remove null typing.
-  const loggedInFullName: string | undefined | null = loggedInMember
+  const loggedInFullName: string | null | undefined = loggedInMember
     ? loggedInMember.fullName
     : loggedInFirebaseUser ? loggedInFirebaseUser.displayName : undefined;
 
@@ -325,7 +291,7 @@ const mapStateToProps: MapStateToProps<StateProps, OwnProps, AppState> = (
 
   return {
     loggedInFirebaseUser,
-    loggedInFullName,
+    loggedInFullName: loggedInFullName || undefined,
 
     requestingFromMember,
 
@@ -358,8 +324,249 @@ const mergeProps: MergeProps<
   };
 };
 
+interface FormElements {
+  age: HTMLInputElement;
+  inactivityDonation: HTMLInputElement;
+  communityStandards: HTMLInputElement;
+  realIdentity: HTMLInputElement;
+  fullName: HTMLInputElement;
+  videoUrl: HTMLInputElement;
+}
 export default connect(
   mapStateToProps,
   { requestInviteFromMember },
   mergeProps
 )(RequestInvite);
+
+const Step0: React.StatelessComponent<{ inviterName: string }> = ({
+  inviterName
+}) => {
+  return (
+    <IntlMessage
+      id="request_invite.step0"
+      values={{ inviter_name: inviterName }}
+    />
+  );
+};
+
+const Step1: React.StatelessComponent<{}> = () => (
+  <IntlMessage id="request_invite.step1" />
+);
+
+const Step2: React.StatelessComponent<{}> = () => (
+  <IntlMessage id="request_invite.step2" />
+);
+
+const Step3: React.StatelessComponent<{ inviterName: string }> = ({
+  inviterName
+}) => (
+  <IntlMessage
+    id="request_invite.step3"
+    values={{ inviter_name: inviterName }}
+  />
+);
+
+interface Step4Props {
+  hasLoggedIn: boolean;
+  videoUploadRef: firebase.storage.Reference;
+}
+
+interface Step4State {
+  videoUrl: string | null;
+}
+
+const RequestInviteForm = styled.form`
+  > .agreements {
+    list-style-type: none;
+    text-align: left;
+    > li {
+      display: flex;
+      margin-bottom: 10px;
+    }
+  }
+`;
+
+class Step4 extends React.Component<Step4Props, Step4State> {
+  constructor(props: Step4Props) {
+    super(props);
+    this.state = {
+      videoUrl: null
+    };
+  }
+
+  private readonly handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formElements: FormElements = e.currentTarget.elements as any;
+    alert(formElements.age.checked);
+  };
+
+  public render() {
+    if (!this.props.hasLoggedIn) {
+      return (
+        <>
+          <FormattedMessage id="sign_up" />
+          <LogIn noRedirect={true} />
+        </>
+      );
+    }
+
+    return (
+      <RequestInviteForm onSubmit={this.handleSubmit}>
+        <ul className="agreements">
+          <li>
+            <input type="checkbox" name="inactivityDonation" />
+            <IntlMessage
+              id="request_invite.agreements.inactivityDonation"
+              onlyRenderText={true}
+            />
+          </li>
+          <li>
+            <input type="checkbox" name="communityStandards" />
+            <IntlMessage
+              id="request_invite.agreements.communityStandards"
+              values={{
+                code_of_conduct: (
+                  <a href="/code-of-conduct">
+                    <IntlMessage
+                      id="request_invite.code_of_conduct"
+                      onlyRenderText={true}
+                    />
+                  </a>
+                ),
+                privacy_policy: (
+                  <a href="/privacy-policy">
+                    <IntlMessage
+                      id="request_invite.privacy_policy"
+                      onlyRenderText={true}
+                    />
+                  </a>
+                ),
+                terms_of_service: (
+                  <a href="/terms-of-service">
+                    <IntlMessage
+                      id="request_invite.terms_of_service"
+                      onlyRenderText={true}
+                    />
+                  </a>
+                )
+              }}
+            />
+          </li>
+          <li>
+            <input type="checkbox" name="realIdentity" />
+            <IntlMessage
+              id="request_invite.agreements.realIdentity"
+              onlyRenderText={true}
+            />
+          </li>
+          <li>
+            <input type="checkbox" name="age" />
+            <IntlMessage
+              id="request_invite.agreements.age"
+              onlyRenderText={true}
+            />
+          </li>
+        </ul>
+
+        <VideoUploader
+          setVideoUrl={videoUrl => this.setState({ videoUrl })}
+          uploadRef={this.props.videoUploadRef}
+        />
+        {this.state.videoUrl && (
+          <>
+            <h2>
+              <FormattedMessage id="join_video" />
+            </h2>
+            <Video videoUrl={this.state.videoUrl} />
+          </>
+        )}
+
+        <Button submit={true}>Submit</Button>
+      </RequestInviteForm>
+    );
+  }
+}
+
+interface WelcomeStepsProps {
+  inviterName: string;
+  hasLoggedIn: boolean;
+  videoUploadRef: firebase.storage.Reference;
+}
+
+interface WelcomeStepsState {
+  currentStep: number;
+}
+
+const WelcomeStepsElem = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 0 20px;
+
+  > .step {
+    margin-bottom: 20px;
+    text-align: center;
+  }
+  > footer {
+    text-align: center;
+  }
+`;
+class WelcomeSteps extends React.Component<
+  WelcomeStepsProps,
+  WelcomeStepsState
+> {
+  constructor(props: WelcomeStepsProps) {
+    super(props);
+
+    this.state = {
+      currentStep: 0
+    };
+  }
+
+  private handleNextClick = () => {
+    this.setState({ currentStep: this.state.currentStep + 1 });
+  };
+
+  private renderCurrentStep = () => {
+    switch (this.state.currentStep) {
+      case 0:
+        return <Step0 inviterName={this.props.inviterName} />;
+      case 1:
+        return <Step1 />;
+      case 2:
+        return <Step2 />;
+      case 3:
+        return <Step3 inviterName={this.props.inviterName} />;
+      case 4:
+        return (
+          <Step4
+            hasLoggedIn={this.props.hasLoggedIn}
+            videoUploadRef={this.props.videoUploadRef}
+          />
+        );
+      default:
+        break;
+    }
+    // TODO: real logging, alerting
+    // tslint:disable-next-line
+    console.error("Invalid step number");
+    return <></>;
+  };
+
+  public render() {
+    return (
+      <WelcomeStepsElem>
+        <section className="step">{this.renderCurrentStep()}</section>
+        {this.state.currentStep < 4 && (
+          <footer>
+            <Button className="nextBtn" onClick={this.handleNextClick}>
+              Next
+            </Button>
+          </footer>
+        )}
+      </WelcomeStepsElem>
+    );
+  }
+}
