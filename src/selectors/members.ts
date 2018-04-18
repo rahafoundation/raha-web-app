41bdc4a42980b060b95f8bb9e979772ea39d5388
Omar Diab <me@omardiab.com>
2018-04-18 18:55:00 -0400

    fix types for getMembers selectors

--- src/selectors/members.ts
+++ src/selectors/members.ts
@@ -2,6 +2,16 @@ import { AppState } from "../store";
 import { Uid } from "../identifiers";
 import { Member } from "../reducers/membersNew";
 
-export function getMembersByUid(state: AppState, uids: Uid[]): Member[] {
+export function getMembersByUid(
+  state: AppState,
+  uids: Uid[]
+): Array<Member | undefined> {
   return uids.map(uid => state.membersNew.byUid[uid]);
+}
+
+export function getMembersByMid(
+  state: AppState,
+  mids: Uid[]
+): Array<Member | undefined> {
+  return mids.map(mid => state.membersNew.byMid[mid]);
 }
