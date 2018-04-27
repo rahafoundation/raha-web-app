151c3729f102376bee25830e9f576a817f317c02
Rahul Gupta-Iwasaki <2237985+rahulgi@users.noreply.github.com>
2018-04-26 23:36:57 -0700

    Send invite emails. (#129)
    * Update InviteInstructions to be able to send email invite.

--- src/api/ApiResponse.ts
+++ src/api/ApiResponse.ts
@@ -3,7 +3,13 @@ import { ApiEndpoint } from "./";
 
 export type OperationApiResponse = Operation;
 export type OperationsApiResponse = Operation[];
+export interface MessageApiResponse {
+  message: string;
+}
 
 // as more response types appear, expand this type
-type ApiResponse = OperationApiResponse | OperationsApiResponse;
+type ApiResponse =
+  | OperationApiResponse
+  | OperationsApiResponse
+  | MessageApiResponse;
 export default ApiResponse;
