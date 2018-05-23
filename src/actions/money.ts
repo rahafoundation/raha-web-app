3667574905088649804e53b332aeaa930a3b8c65
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-05-23 16:32:06 -0700

    Add Give ApiCall and Operation handling.

--- src/actions/money.ts
+++ src/actions/money.ts
@@ -1,6 +1,6 @@
 import { AppState } from "../store";
 
-import { ApiEndpoint, callApi, MintApiEndpoint } from "../api";
+import { ApiEndpoint, callApi, MintApiEndpoint, GiveApiEndpoint } from "../api";
 import { Uid } from "../identifiers";
 import { getAuthToken } from "../selectors/auth";
 import UnauthenticatedError from "../errors/ApiCallError/UnauthenticatedError";
@@ -33,6 +33,41 @@ export const mint: AsyncActionCreator = (uid: Uid, amount: string) => {
       dispatch(action);
     },
     ApiEndpoint.MINT,
+    uid
+  );
+};
+
+export const give: AsyncActionCreator = (
+  uid: Uid,
+  amount: string,
+  memo: string
+) => {
+  return wrapApiCallAction(
+    async (dispatch, getState) => {
+      const authToken = await getAuthToken(getState());
+      if (!authToken) {
+        throw new UnauthenticatedError();
+      }
+
+      const response = await callApi<GiveApiEndpoint>(
+        {
+          endpoint: ApiEndpoint.GIVE,
+          params: { uid },
+          body: {
+            amount,
+            memo
+          }
+        },
+        authToken
+      );
+
+      const action: OperationsAction = {
+        type: OperationsActionType.ADD_OPERATIONS,
+        operations: [response]
+      };
+      dispatch(action);
+    },
+    ApiEndpoint.GIVE,
     uid
   );
 };
