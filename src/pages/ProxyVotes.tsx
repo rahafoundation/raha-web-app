702d0037b6e1517ff6b535e329fd596c819289c9
Mark Ulrich <markulrich@users.noreply.github.com>
2018-05-24 19:27:49 -0700

    Emphasize experimental in title and improve style

--- src/pages/ProxyVotes.tsx
+++ src/pages/ProxyVotes.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 
 import IntlMessage from "../components/IntlMessage";
 import { AppState } from "../reducers";
+import { Uid } from "../identifiers";
 import {
   Member,
   MemberLookupTable,
@@ -21,9 +22,6 @@ interface Props {
 
 const ProxyVotesElem = styled.main`
   max-width: 90vw;
-  display: flex;
-  align-items: center;
-  flex-direction: column;
 `;
 
 // TODO these should be nice thumbnails presented in a component also used by Leaderboard.tsx
@@ -50,46 +48,44 @@ const ProxyVotes: React.StatelessComponent<Props> = ({ memberAndVotes }) => {
 
 function getAncestors(
   member: Member,
-  membersByUid: MemberLookupTable,
-  visited: Set<string> = new Set()
-): Set<string> {
-  const uid = member.uid;
-  if (visited.has(uid)) {
-    throw Error(`Found cycle containing member uid ${uid}`);
+  membersByUid: MemberLookupTable
+): Set<Uid> {
+  // Everyone is their own ancestor in this function
+  let votingForMember = member;
+  const ancestors = new Set<Uid>();
+  for (
+    let votingForUid: Uid | typeof GENESIS_MEMBER = votingForMember.uid;
+    votingForUid !== GENESIS_MEMBER; // TODO needs to change to check if voting for self
+    votingForUid = votingForMember.invitedBy  // TODO needs to change to votingFor
+  ) {
+    votingForMember = membersByUid[votingForUid];
+    if (votingForMember === undefined) {
+      throw Error(`Cannot vote for invalid uid ${votingForUid}`);
+    }
+    if (ancestors.has(votingForUid)) {
+      throw Error(`Cycle in ancestors of ${member.uid}: ${votingForUid} found twice`);
+    }
+    ancestors.add(votingForUid);
   }
-  visited.add(uid);
-  const votingForUid = member.invitedBy; // TODO needs to change to votingFor
-  if (votingForUid === GENESIS_MEMBER) {
-    // TODO needs to change to check if voting for self
-    return visited;
-  }
-  const votingForMember = membersByUid[votingForUid];
-  if (votingForUid === undefined) {
-    throw Error(`Cannot vote for invalid uid ${votingForUid}`);
-  }
-  return getAncestors(votingForMember, membersByUid, visited);
+  return ancestors;
 }
 
-function incrementKey(counts: Map<string, number>, key: string) {
-  let count = counts.get(key);
-  if (count === undefined) {
-    count = 0;
-  }
-  count++;
-  counts.set(key, count);
+function incrementKey(counts: Map<any, number>, key: any) {
+  const count = counts.get(key);
+  counts.set(key, count === undefined ? 1 : count + 1);
 }
 
 function incVotesForAncestors(
   member: Member,
   membersByUid: MemberLookupTable,
-  votesByUid: Map<string, number>
+  votesByUid: Map<Uid, number>
 ) {
   const ancestors = getAncestors(member, membersByUid);
   ancestors.forEach(a => incrementKey(votesByUid, a));
 }
 
 function getVotesByUid(membersByUid: MemberLookupTable) {
-  const votesByUid = new Map();
+  const votesByUid = new Map<Uid, number>();
   Object.values(membersByUid).forEach(m =>
     incVotesForAncestors(m, membersByUid, votesByUid)
   );
