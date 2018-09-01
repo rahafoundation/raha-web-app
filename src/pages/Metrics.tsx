b8fbce88bde525343423299eb748911e0a24229c
Mark Ulrich <markulrich@users.noreply.github.com>
2018-09-01 01:29:46 -0500

    Add WAC and show change

--- src/pages/Metrics.tsx
+++ src/pages/Metrics.tsx
@@ -16,21 +16,36 @@ interface StateProps {
 
 type Props = OwnProps & StateProps;
 
+function getDaysAgo(numDays: number, date?: Date) {
+  const daysAgo = date ? new Date(date.getTime()) : new Date();
+  daysAgo.setDate(daysAgo.getDate() - numDays);
+  return daysAgo;
+}
+
 /**
  * TODO this calculation should really be a cached (reselect?) connector.
- * TODO date and duration parameters.
  *
- * Returns MAC, our "monthly active creators" metric which
+ * Returns "active creators" over a given duration which
  * means the number of verified users who have created a
  * public operation within the last 30 days.
  */
-function getMac(operations: Operation[], members: Map<MemberId, Member>) {
-  const lastMonth = new Date();
-  lastMonth.setDate(lastMonth.getDate() - 30);
+function getActiveCreators(
+  operations: Operation[],
+  members: Map<MemberId, Member>,
+  durationDays: number,
+  endDate: Date
+) {
+  const startDate = new Date(endDate.getTime());
+  startDate.setDate(startDate.getDate() - durationDays);
   const memberIdSet = new Set();
   for (let i = operations.length - 1; i >= 0; i--) {
     const op = operations[i];
-    if (new Date(op.created_at) < lastMonth) {
+    const createdAt = new Date(op.created_at);
+    if (createdAt > endDate) {
+      // TODO Inefficent - use binary search
+      continue;
+    }
+    if (createdAt < startDate) {
       break;
     }
     const member = members.get(op.creator_uid);
@@ -41,20 +56,66 @@ function getMac(operations: Operation[], members: Map<MemberId, Member>) {
   return memberIdSet.size;
 }
 
+function alertIfNotChronological(operations: Operation[]) {
+  let prevTime = new Date(operations[0].created_at);
+  for (let i = 1; i < operations.length; i++) {
+    const currTime = new Date(operations[i].created_at);
+    if (currTime < prevTime) {
+      alert(
+        "Sorry operations are out of order, metrics may be incorrect. Please email bugs@raha.app."
+      );
+      return;
+    }
+    prevTime = currTime;
+  }
+}
+
+function getCurrAndLastActiveCreators(
+  operations: Operation[],
+  members: Map<MemberId, Member>,
+  durationDays: number
+) {
+  const now = new Date();
+  const curr = getActiveCreators(operations, members, durationDays, now);
+  const last = getActiveCreators(
+    operations,
+    members,
+    durationDays,
+    getDaysAgo(durationDays, now)
+  );
+  const change = `${((curr / last - 1) * 100).toFixed(2)}%`;
+  const description = `How many verified members have created at least one operation in the
+  last ${durationDays} days?`;
+  return [curr, last, change, description];
+}
+
 // TODO improve styling, don't rely on h1/h2/h3, add graphs
 const MetricsView: React.StatelessComponent<Props> = props => {
   const { members, operations } = props;
   if (!operations || operations.length === 0) {
     return <Loading />;
   }
+  alertIfNotChronological(operations);
+  const [currMac, lastMac, changeMac, descMac] = getCurrAndLastActiveCreators(
+    operations,
+    members,
+    30
+  );
+  const [currWac, lastWac, changeWac, descWac] = getCurrAndLastActiveCreators(
+    operations,
+    members,
+    7
+  );
   return (
     <section style={{ margin: "20px" }}>
-      <h2>MAC (Monthly Active Creators)</h2>
-      <h3>
-        How many verified members have created at least one operation in the
-        last 30 days?
-      </h3>
-      <h1>{getMac(operations, members)}</h1>
+      <h1 title={descMac as string}>{currMac} MAC Monthly Active Creators</h1>
+      <h2>
+        {changeMac} change from last month ({lastMac})
+      </h2>
+      <h1 title={descWac as string}>{currWac} WAC Weekly Active Creators</h1>
+      <h2>
+        {changeWac} change from last week ({lastWac})
+      </h2>
     </section>
   );
 };
