ce4c1e39003839a748b7ce17581ab3214a5f2c10
Tina Roh <tinaroh@gmail.com>
2018-09-01 23:10:37 +0900

    Update to include iOS

--- src/pages/AcceptInvite.tsx
+++ src/pages/AcceptInvite.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import { IntlMessage } from "../components/IntlMessage";
 
 const AcceptInvite: React.StatelessComponent<{}> = () => (
-  <div className="AcceptInvite">
-    <IntlMessage id="accept_invite" />
-    <p>
+  <div className="AcceptInviteSteps">
+    <div className="AcceptInviteStep">
+      <h3>
+        <IntlMessage id="accept_invite_step1" />
+      </h3>
       <a href="https://play.google.com/store/apps/details?id=app.raha.mobile">
         <img
           width="300"
@@ -13,23 +15,31 @@ const AcceptInvite: React.StatelessComponent<{}> = () => (
           src="https://play.google.com/intl/en_us/badges/images/generic/en_badge_web_generic.png"
         />
       </a>
-      {/* <br />
-      <a href="https://play.google.com/store/apps/details?id=app.raha.mobile">
+      <br />
+      <a href="https://itunes.apple.com/app/raha/id1434224783?ls=1&mt=8">
         <img
           width="250"
-          alt="Get it on Apple Store"
-          src="/apple_app_store.svg"
+          alt="Download on the Apple App Store"
+          src="https://linkmaker.itunes.apple.com/assets/shared/badges/en-us/appstore-lrg.svg"
         />
-      </a> */}
-    </p>
-    <p style={{ fontSize: 12 }}>
-      Google Play and the Google Play logo are trademarks of Google LLC.
-    </p>
-    {/* <p style={{ fontSize: 12 }}>
-      Apple and the Apple logo are trademarks of Apple Inc., registered in the
-      U.S. and other countries and regions. App Store is a service mark of Apple
-      Inc.
-    </p> */}
+      </a>
+      <p style={{ fontSize: 12 }}>
+        Google Play and the Google Play logo are trademarks of Google LLC.
+      </p>
+      <p style={{ fontSize: 12 }}>
+        Apple and the Apple logo are trademarks of Apple Inc., registered in the
+        U.S. and other countries and regions. App Store is a service mark of
+        Apple Inc.
+      </p>
+    </div>
+    <div className="AcceptInviteStep">
+      <h3>
+        <IntlMessage id="accept_invite_step2" />
+      </h3>
+      <a style={{ fontSize: 18 }} href={window.location.href}>
+        {window.location.href}
+      </a>
+    </div>
   </div>
 );
 
