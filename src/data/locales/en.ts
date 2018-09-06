ceb0da0fa292faea941db90b7932ff02af653275
Rahul Gupta-Iwasaki <rahul.guptaiwasaki@gmail.com>
2018-09-06 15:51:20 +0900

    Remove unused text.

--- src/data/locales/en.ts
+++ src/data/locales/en.ts
@@ -1,51 +1,4 @@
 export const messages = {
-  "profile.trustButton": "Trust",
-  "profile.trustedMember": "Trusted",
-  "profile.trustFailure": "Trusting failed, try again",
-  "profile.memberNotFound": "Member with username {memberUsername} not found",
-  "profile.viewWallet": "View wallet (beta)",
-
-  "request_invite.failed": "Requesting invite failed; please try again.",
-  "request_invite.step0":
-    "{inviter_name} invited you to Raha. Let's get you started so you can begin receiving your Raha Basic Income.",
-  "request_invite.step1":
-    "We believe every life has value, but inequality has become the defining problem of our generation.",
-  "request_invite.step2":
-    "Members receive a Raha Basic Income, paid in Raha points. In exchange, when you are inactive for a year or more then your balance is donated back to the network.",
-  "request_invite.step3":
-    "Raha is free to join with no transaction fees. Talk to {inviter_name} to verify your identity with a video!",
-  "request_invite.agreements.inactivityDonation":
-    "I understand and agree that after 1 year of inactivity all of my Raha will be irrevocably and irretrievably donated to fund basic income, with 80% going directly to members and 20% to the member-owned Raha Parliament.",
-  "request_invite.agreements.communityStandards":
-    "I agree to abide by the following community standards:",
-  "request_invite.agreements.realIdentity":
-    "This is my real identity, my full name, and the only time I have joined Raha. I understand that creating duplicate or fake accounts may result in me and people I have invited losing access to our accounts.",
-  "request_invite.agreements.age": "I am at least 13 years old.",
-
-  "wallet.title": "Raha Wallet (Beta)",
-  "wallet.balance.message": "Your balance: {balance} Raha",
-  "wallet.basicIncome.title": "Your basic income",
-  "wallet.basicIncome.detail":
-    'Every member of the Raha network gets to "mint" their own basic income. The amount of Raha you can mint accrues at a rate of 10 Raha per week.',
-  "wallet.basicIncome.lastMinted":
-    "You last minted your Raha on {lastMintedDate} at {lastMintedTime}.",
-  "wallet.basicIncome.clickPrompt":
-    "Click the button below to mint {mintableAmount} Raha!",
-  "wallet.basicIncome.alreadyClaimed":
-    "You've minted all of your basic income for now. Come back later to mint more!",
-  "wallet.basicIncome.mintButton": "Mint {mintableAmount} Raha",
-  "wallet.basicIncome.inviteConfirmationRequired":
-    "Your invite request must be confirmed before you can mint your basic income.",
-  "wallet.give.detail":
-    "{fullName} is currently donating {donationPercent} of all Raha they receive back to the Raha basic income pool. This donation will be used to fund future basic income distributions for everyone in the Raha network.",
-  "wallet.give.amountLabel": "Amount to give:",
-  "wallet.give.amountPlaceholder": "Amount to give",
-  "wallet.give.memoLabel": "Say why:",
-  "wallet.give.memoPlaceholder": "for being so amazing!",
-  "wallet.give.memoHelper": "{charactersRemaining} characters remaining",
-  "wallet.give.giveButton": "Give {amount} Raha to {fullName}{memo}",
-  "wallet.feed.header": "Activity Feed",
-
   "operationList.createMember": "{fullName} joined Raha",
   "operationList.verify": "{verifierName} verified {verifiedName}'s profile",
   "operationList.requestInvite": "{fromName} requested an invite from {toName}",
@@ -57,40 +10,16 @@ export const messages = {
 
   "app_layout.log_in": "Log In",
   completely_free: "completely free",
-  invite_instructions_header: "Invite your friends to join Raha!",
-  invite_instructions_send_email_label:
-    "Send them an email with your invite link",
-  invite_instructions_send_email_button: "Send invite",
-  invite_instructions_direct_link_label:
-    "Or, have them visit this link directly",
-  invite_instructions_email_sent_message: "Invite sent!",
-  invite_Instructions_email_failed_message: "Sending failed.",
   invite_pending:
     "Hi {full_name}! You have requested an invite from {invite_from_link}. After they trust you and the community approves, you will become a full member! Contact {help_email} with any questions.",
-  invited: "Invited",
-  invited_by: "Invited by",
   loading: "Loading",
   invite_missing:
     "Thank you for logging in {display_name}! Your account is inactive, to become a member you need a member to create an invite video with you. Contact {help_email} with any questions. You are logged in using account {login_account}, if this is wrong please {logout}.",
-  trusted_by: "Trusted by",
-  trusts: "Trusts",
-  join_video: "Preview of your public join video",
   own_invite_page: "This is what your invite page looks like to others:",
   page_not_found: "{404} page not found, go {home}.",
   upload_invite: "Upload your public invite video! Max 60 MB.",
   sign_up:
     "Sign up below to continue. We do not ask for your contact's information or ability to post.",
-  "request_invite.code_of_conduct": "Code of Conduct",
-  "request_invite.privacy_policy": "Privacy Policy",
-  "request_invite.terms_of_service": "Terms of Service",
-  "request_invite.terms":
-    "By requesting an invite, I agree to abide by the {code_of_conduct}, {privacy_policy}, and {terms_of_service}.",
-  identity_suggestion_active_longer:
-    "Stay active on the network for more days to increase your identity level.",
-  identity_suggestion_recieve_trust:
-    "Increase your trust by receiving trust from {accounts} level {accountlevel}+ accounts.",
-  identity_suggestion_max: "You are max identity level.",
-  identity_level: "Identity Level",
   "votes.votes": "Experimental Votes",
   "votes.heading":
     "Number of proxy votes in Raha by request invite. This is the number of proxy votes currently controlled by the member assuming 0 vote changes by any descendants, currently has no effect. The goal of this experiment would be to create a system where everyone's vote has equal value, but early adopters and enthusiastic network supporters have an advantage in early stages. Long-term, proxy voting could encourage people to give their vote to friends or thought-leaders with similar values who are carefully researching the issues instead of listening to headlines and advertisements.",
